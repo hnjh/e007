6f8d8de3bfeb05a8665dcbfb53f61e404b4ce5b8
yatung hsieh <yatung.hsieh@gmail.com>
2019-09-09 18:56:19 +0800

    modified:   apps/e007/src/e007.app.src 	modified:   apps/e007_server/src/es_accesstokens_handler.erl

--- apps/e007_server/src/es_accesstokens_handler.erl
+++ apps/e007_server/src/es_accesstokens_handler.erl
@@ -3,16 +3,32 @@
 -behaviour(cowboy_rest).
 
 -export([init/2]).
+-export([allowed_methods/2]).
 -export([content_types_provided/2]).
--export([to_html/2]).
+-export([content_types_accepted/2]).
+-export([resource_exists/2]).
+-export([to_json/2]).
 
 init(Req, State) ->
   {cowboy_rest, Req, State}.
 
-content_types_provided(Req, State) ->
+allowed_methods(Req, State) ->
+  AllowedMethods = [<<"POST">>, <<"DELETE">>],
+  {AllowedMethods, Req, State}.
+
+content_types_accepted(Req, State) ->
   {[
-    {{<<"text">>, <<"html">>, '*'}, to_html}
+    {{<<"application">>, <<"json">>, '*'}, to_json}
   ], Req, State}.
 
-to_html(Req, State) ->
-  {<<"<html><body>This is REST!</body></html>">>, Req, State}.
+content_types_provided(Req, State) ->
+  {[
+    {{<<"application">>, <<"json">>, '*'}, to_json}
+  ], Req, State}.
+
+to_json(Req, State) ->
+  {<<"{}">>, Req, State}.
+
+resource_exists(Req, State) ->
+  Method = cowboy_req:method(Req),
+  {Method =/= <<"POST">>, Req, State}.
